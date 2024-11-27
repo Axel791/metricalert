6bcbdb9ddbbc1b3027edf4ab7e2755eae5f1e19a
timofejkazakov <t.kazakov@agora.ru>
2024-11-27 22:48:07 +0300

    Добаивл в моки функцию

--- internal/storage/mocks/store_mock.go
+++ internal/storage/mocks/store_mock.go
@@ -23,3 +23,11 @@ func (m *MockStore) UpdateCounter(name string, value int64) int64 {
 	}
 	return 0
 }
+
+func (m *MockStore) GetMetric(name string) interface{} {
+	args := m.Called(name)
+	if result := args.Get(0); result != nil {
+		return result
+	}
+	return 0
+}
