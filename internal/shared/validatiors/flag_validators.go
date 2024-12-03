d42e36452ac138bc700084fd1d9ef7232768cf8f
timofejkazakov <t.kazakov@agora.ru>
2024-12-03 18:15:31 +0300

    Проверка

--- internal/shared/validatiors/flag_validators.go
+++ internal/shared/validatiors/flag_validators.go
@@ -1,15 +1,36 @@
 package validatiors
 
-import "regexp"
+import (
+	"net"
+	"strings"
+)
 
 // IsValidAddress проверяет валидность адреса.
 // Если withScheme == true, учитывается схема (http:// или https://).
 func IsValidAddress(addr string, withScheme bool) bool {
-	var re *regexp.Regexp
 	if withScheme {
-		re = regexp.MustCompile(`^(http://|https://)[a-zA-Z0-9.-]+:[0-9]{1,5}$`)
-	} else {
-		re = regexp.MustCompile(`^[a-zA-Z0-9.-]+:[0-9]{1,5}$`)
+		addr = strings.TrimPrefix(addr, "http://")
+		addr = strings.TrimPrefix(addr, "https://")
 	}
-	return re.MatchString(addr)
+
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return false
+	}
+
+	if _, err := net.LookupPort("tcp", port); err != nil {
+		return false
+	}
+	if host == "" {
+		return false
+	}
+
+	if _, err := net.LookupHost(host); err != nil {
+		ip := net.ParseIP(host)
+		if ip == nil {
+			return false
+		}
+	}
+
+	return true
 }
